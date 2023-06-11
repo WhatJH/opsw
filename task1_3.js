7405b6868a7cbe3e2a8c1d8b92439e8ce58c412f
WhatJH <127819165+WhatJH@users.noreply.github.com>
2023-06-11 22:11:53 +0900

    Add files via upload

--- task1_3.js
+++ task1_3.js
@@ -1,64 +1,360 @@
-// 글쓰기 주제를 1초마다 변경하는 함수
-function changeTopic() {
-    const topics = ["오늘 점심 추천", "커피 추천"]; // 변경될 주제들
-    let currentTopicIndex = 0;
-    const topicElement = document.getElementById("topic");
-  
-    setInterval(() => {
-      currentTopicIndex = (currentTopicIndex + 1) % topics.length;
-      topicElement.textContent = topics[currentTopicIndex];
-    }, 1000);
+function validateForm(){
+  var name = document.getElementById("name").value;
+  var age = document.getElementById("age").value;
+  var address = document.getElementById("address").value;
+  var email = document.getElementById("email").value;
+
+  if(name == ""){
+    alert("Name is required");
+    return false;
   }
-  
-  // 글 작성 폼 제출 시 처리하는 함수
-  function submitForm(event) {
-    event.preventDefault(); // 폼 제출 시 페이지 이동 방지
-  
-    const foodType = document.getElementById("food-type").value;
-    const reason = document.getElementById("reason").value;
-    const currentDate = new Date().toLocaleString();
-    const postElement = document.createElement("div");
-  
-    postElement.innerHTML = `
-      <p>${foodType}을(를) 추천합니다.</p>
-      <p>이유: ${reason}</p>
-      <p>글쓴이: <span class="author">홍길동</span></p>
-      <p>작성 시간: ${currentDate}</p>
-      <button class="edit">수정</button>
-      <button class="delete">삭제</button>
-    `;
-  
-    document.getElementById("posts").appendChild(postElement);
-  
-    // 작성된 글에 수정 및 삭제 기능 추가
-    const editButton = postElement.querySelector(".edit");
-    const deleteButton = postElement.querySelector(".delete");
-  
-    editButton.addEventListener("click", () => {
-      // 수정 기능 구현
-    });
-  
-    deleteButton.addEventListener("click", () => {
-      postElement.remove();
-    });
-  
-    // 폼 초기화
-    document.getElementById("food-type").value = "한식";
-    document.getElementById("reason").value = "";
+  if(age == ""){
+    alert("Age is required");
+    return false;
   }
-  
-  // 화면이 로드되면 실행되는 함수
-  window.addEventListener("load", () => {
-    changeTopic(); // 글쓰기 주제 변경
-  
-    // 글 작성 폼 제출 이벤트 처리
-    const recommendationForm = document.getElementById("recommendation-form");
-    recommendationForm.addEventListener("submit", submitForm);
-  
-    // 확인 버튼 클릭 시 작업 수행 (여기서는 alert 메시지 출력)
-    const confirmButton = document.getElementById("confirm");
-    confirmButton.addEventListener("click", () => {
-      alert("확인 버튼을 클릭했습니다.");
-    });
+  else if(age <= 1){
+    alert("Proper age is required");
+    return false;
+  }
+  if(address == ""){
+    alert("Address is required");
+    return false;
+  }
+  if(email == ""){
+    alert("Email is required");
+  }
+  else if(!email.includes("@")){
+    alert("Invalid email address");
+    return false;
+  }
+
+  return true;
+}
+
+function showData(){
+  var peopleList;
+  if(localStorage.getItem("peopleList")==null){
+    peopleList=[];
+  }
+  else{
+    peopleList=JSON.parse(localStorage.getItem("peopleList"));
+  }
+  var html="";
+
+  peopleList.forEach(function(element,index){
+    html += "<tr>";
+    html += "<td>"+ element.name + "</td>";
+    html += "<td>"+ element.age + "</td>";
+    html += "<td>"+ element.address + "</td>";
+    html += "<td>"+ element.email + "</td>";
+    html +=
+  '<td><button onclick="deleteData(' +
+  index +
+  ')" class="btn btn-danger">Delete</button>' + 
+  '<button onclick="updateData(' +
+  index +
+  ')" class="btn btn-warning m-2">Edit</button></td>';
+html += "</tr>";
+});
+  document.querySelector("#crudTable tbody").innerHTML=
+   html;
+  }
+
+  window.onload = showData;
+
+
+  function AddData(){
+    if (validateForm() == true){
+      var name = document.getElementById("name").value;
+      var age = document.getElementById("age").value;
+      var address = document.getElementById("address").value;
+      var email = document.getElementById("email").value;
+    
+    var peopleList;
+    if(localStorage.getItem("peopleList") == null) {
+      peopleList=[];
+    } else {
+      peopleList=JSON.parse(localStorage.getItem("peopleList"));
+    }
+
+    peopleList.push({
+      name: name,
+      age: age,
+      address: address,
+      email: email,
+      commets:[]
+
   });
-  
+
+  localStorage.setItem("peopleList",JSON.stringify
+  (peopleList));
+  showData();
+  document.getElementById("name").value ="";
+  document.getElementById("age").value ="";
+  document.getElementById("address").value="";
+  document.getElementById("email").value="";
+ }
+}
+function deleteData(index){
+  var peopleList;
+    if(localStorage.getItem("peopleList")==null){
+      peopleList=[];
+    }
+    else{
+      peopleList=JSON.parse(localStorage.getItem("peopleList"));
+    }
+
+    peopleList.splice(index,1);
+    localStorage.setItem("peopleList",JSON.stringify
+    (peopleList));
+    showData();
+  }
+
+
+function updateData(index) {
+  document.getElementById("Submit").style.display = "none";
+  document.getElementById("Update").style.display = "block";
+
+  var peopleList;
+  if (localStorage.getItem("peopleList") == null) {
+    peopleList = [];
+  } else {
+    peopleList = JSON.parse(localStorage.getItem("peopleList"));
+  }
+
+  document.getElementById("name").value = peopleList[index].name;
+  document.getElementById("age").value = peopleList[index].age;
+  document.getElementById("address").value = peopleList[index].address;
+  document.getElementById("email").value = peopleList[index].email;
+
+  document.querySelector("#Update").onclick = function() {
+    if (validateForm() == true) {
+      peopleList[index].name = document.getElementById("name").value;
+      peopleList[index].age = document.getElementById("age").value;
+      peopleList[index].address = document.getElementById("address").value;
+      peopleList[index].email = document.getElementById("email").value;
+
+      localStorage.setItem("peopleList", JSON.stringify(peopleList));
+
+      showData();
+
+      document.getElementById("name").value = "";
+      document.getElementById("age").value = "";
+      document.getElementById("address").value = "";
+      document.getElementById("email").value = "";
+
+      document.getElementById("Submit").style.display = "block";
+      document.getElementById("Update").style.display = "none";
+    }};}
+    var comments = [];
+
+ function addComment() {
+      var commentText = document.getElementById("comment").value;
+      if (commentText.trim() === "") {
+        alert("댓글을 입력해주세요.");
+        return;
+      }
+    
+      var newComment = {
+        id: comments.length + 1,
+        text: commentText,
+        isEditing: false
+      };
+    
+      comments.push(newComment);
+      renderComments();
+      document.getElementById("comment").value = "";
+    }
+    
+    function deleteComment(id) {
+      var commentIndex = comments.findIndex(function(comment) {
+        return comment.id === id;
+      });
+    
+      if (commentIndex !== -1) {
+        comments.splice(commentIndex, 1);
+        renderComments();
+      }
+    }
+    
+function saveComment(id) {
+      var commentIndex = comments.findIndex(function(comment) {
+        return comment.id === id;
+      });
+    
+      if (commentIndex !== -1) {
+        var newText = document.getElementById("commentText_" + id).value;
+        if (newText.trim() === "") {
+          alert("댓글을 입력해주세요.");
+          return;
+        }
+        comments[commentIndex].text = newText;
+        comments[commentIndex].isEditing = false;
+        renderComments();
+      }
+    }
+    
+    function cancelEdit(id) {
+      var commentIndex = comments.findIndex(function(comment) {
+        return comment.id === id;
+      });
+    
+      if (commentIndex !== -1) {
+        comments[commentIndex].isEditing = false;
+        renderComments();
+      }
+    }
+    var comments = [];
+
+    function addComment() {
+      var commentText = document.getElementById("comment").value;
+      if (commentText.trim() === "") {
+        alert("댓글을 입력해주세요.");
+        return;
+      }
+    
+      var newComment = {
+        id: comments.length + 1,
+        text: commentText,
+        isEditing: false
+      };
+    
+      comments.push(newComment);
+      renderComments();
+      document.getElementById("comment").value = "";
+    }
+    
+    function deleteComment(id) {
+      var commentIndex = comments.findIndex(function(comment) {
+        return comment.id === id;
+      });
+    
+      if (commentIndex !== -1) {
+        comments.splice(commentIndex, 1);
+        renderComments();
+      }
+    }
+    
+    function editComment(id) {
+      var commentIndex = comments.findIndex(function(comment) {
+        return comment.id === id;
+      });
+    
+      if (commentIndex !== -1) {
+        comments[commentIndex].isEditing = true;
+        renderComments();
+      }
+    }
+    
+    function saveComment(id) {
+      var commentIndex = comments.findIndex(function(comment) {
+        return comment.id === id;
+      });
+    
+      if (commentIndex !== -1) {
+        var newText = document.getElementById("commentText_" + id).value;
+        if (newText.trim() === "") {
+          alert("댓글을 입력해주세요.");
+          return;
+        }
+        comments[commentIndex].text = newText;
+        comments[commentIndex].isEditing = false;
+        renderComments();
+      }
+    }
+    
+    function cancelEdit(id) {
+      var commentIndex = comments.findIndex(function(comment) {
+        return comment.id === id;
+      });
+    
+      if (commentIndex !== -1) {
+        comments[commentIndex].isEditing = false;
+        renderComments();
+      }
+    }
+    
+    function renderComments() {
+      var commentSection = document.getElementById("commentSection");
+      commentSection.innerHTML = "";
+    
+      comments.forEach(function(comment) {
+        var commentElement = document.createElement("div");
+        commentElement.className = "comment";
+    
+        if (comment.isEditing) {
+          var commentInput = document.createElement("textarea");
+          commentInput.id = "commentText_" + comment.id;
+          commentInput.value = comment.text;
+    
+          var saveButton = document.createElement("button");
+          saveButton.className = "btn btn-primary btn-sm";
+          saveButton.textContent = "저장";
+          saveButton.onclick = function() {
+            saveComment(comment.id);
+          };
+    
+          var cancelButton = document.createElement("button");
+          cancelButton.className = "btn btn-secondary btn-sm";
+          cancelButton.textContent = "취소";
+          cancelButton.onclick = function() {
+            cancelEdit(comment.id);
+          };
+    
+          commentElement.appendChild(commentInput);
+          commentElement.appendChild(saveButton);
+          commentElement.appendChild(cancelButton);
+        } else {
+          var commentText = document.createElement("p");
+          commentText.textContent = comment.text;
+          commentText.id = "commentText_" + comment.id;
+    
+          var editButton = document.createElement("button");
+          editButton.className = "btn btn-info btn-sm";
+          editButton.textContent = "수정";
+          editButton.onclick = function() {
+            editComment(comment.id);
+          };
+    
+          var deleteButton = document.createElement("button");
+          deleteButton.className = "btn btn-danger btn-sm";
+          deleteButton.textContent = "삭제";
+          deleteButton.onclick = function() {
+            deleteComment(comment.id);
+          };
+    
+          commentElement.appendChild(commentText);
+          commentElement.appendChild(editButton);
+          commentElement.appendChild(deleteButton);
+        }
+    
+        commentSection.appendChild(commentElement);
+      });
+    }
+    
+    function searchTable() {
+      // 검색어 가져오기
+      var searchText = document.getElementById("search").value.toLowerCase();
+    
+      // 테이블 행 가져오기
+      var table = document.getElementById("crudTable");
+      var rows = table.getElementsByTagName("tr");
+    
+      // 테이블 행 반복
+      for (var i = 1; i < rows.length; i++) { // i=0은 테이블의 헤더이므로 제외
+        var nameColumn = rows[i].getElementsByTagName("td")[0]; // 이름 열 가져오기
+        var name = nameColumn.textContent.toLowerCase(); // 이름 열의 텍스트 가져오기
+    
+        // 이름에 검색어가 포함되어 있는지 확인
+        if (name.indexOf(searchText) > -1) {
+          rows[i].style.display = ""; // 행 보이기
+        } else {
+          rows[i].style.display = "none"; // 행 숨기기
+        }
+      }
+    }
+    
+    // 검색 버튼 클릭 시 검색 기능 실행
+    document.getElementById("searchBtn").addEventListener("click", searchTable);
+    
+
--- task2_3.js
+++ task2_3.js
@@ -1,22 +1,180 @@
+var comments = [];
 
-  // 방명록 작성 폼의 submit 이벤트 핸들러
-  document.getElementById("guestbook-form").addEventListener("submit", function(event) {
-    event.preventDefault(); // 폼의 기본 동작인 페이지 새로고침을 방지합니다.
-    
-    // 작성자와 메시지 내용 가져오기
-    const author = document.getElementById("guestbook-author").value;
-    const message = document.getElementById("guestbook-message").value;
+// 새로운 댓글 추가
+function addComment() {
+  var author = document.getElementById("author").value;
+  var commentText = document.getElementById("comment").value;
 
-    // 작성된 방명록 게시물을 HTML로 생성합니다.
-    const postElement = document.createElement("div");
-    postElement.innerHTML = `<p><strong>${author}:</strong> ${message}</p>`;
+  if (author.trim() === "" && commentText.trim() === "") {
+    alert("빈칸을 모두 입력해주세요.");
+    return;
+  }
 
-    // 방명록 게시물을 추가합니다.
-    const guestbookPostsElement = document.getElementById("guestbook-posts");
-    guestbookPostsElement.appendChild(postElement);
+  if (author.trim() === "") {
+    alert("닉네임을 입력해주세요.");
+    return;
+  }
 
-    // 작성 후 입력 필드 초기화
-    document.getElementById("guestbook-author").value = "";
-    document.getElementById("guestbook-message").value = "";
+  if (commentText.trim() === "") {
+    alert("내용을 입력해주세요.");
+    return;
+  }
+
+  var newComment = {
+    id: comments.length + 1,
+    author: author,
+    text: commentText,
+    isEditing: false
+  };
+
+  comments.push(newComment);
+  saveComments(); // 댓글 저장
+  renderComments();
+  document.getElementById("author").value = "";
+  document.getElementById("comment").value = "";
+}
+
+// 댓글 삭제
+function deleteComment(id) {
+  var commentIndex = comments.findIndex(function (comment) {
+    return comment.id === id;
   });
 
+  if (commentIndex !== -1) {
+    comments.splice(commentIndex, 1);
+    saveComments(); // 댓글 저장
+    renderComments();
+  }
+}
+
+// 댓글 수정
+function editComment(id) {
+  var commentIndex = comments.findIndex(function (comment) {
+    return comment.id === id;
+  });
+
+  if (commentIndex !== -1) {
+    comments[commentIndex].isEditing = true;
+    renderComments();
+  }
+}
+
+// 수정된 댓글 저장
+function saveComment(id) {
+  var commentIndex = comments.findIndex(function (comment) {
+    return comment.id === id;
+  });
+
+  if (commentIndex !== -1) {
+    var newText = document.getElementById("commentText_" + id).value;
+    if (newText.trim() === "") {
+      alert("내용을 입력해주세요.");
+      return;
+    }
+    comments[commentIndex].text = newText;
+    comments[commentIndex].isEditing = false;
+    saveComments(); // 댓글 저장
+    renderComments();
+  }
+}
+
+// 수정 취소
+function cancelEdit(id) {
+  var commentIndex = comments.findIndex(function (comment) {
+    return comment.id === id;
+  });
+
+  if (commentIndex !== -1) {
+    comments[commentIndex].isEditing = false;
+    renderComments();
+  }
+}
+
+// 댓글 저장
+function saveComments() {
+  localStorage.setItem("comments", JSON.stringify(comments)); // 댓글을 JSON 문자열로 변환하여 저장
+}
+
+// 저장된 댓글 불러오기
+function loadComments() {
+  var storedComments = localStorage.getItem("comments");
+  if (storedComments) {
+    comments = JSON.parse(storedComments); // JSON 문자열을 파싱하여 댓글 배열로 변환
+  }
+}
+
+// 댓글 렌더링
+function renderComments() {
+  var commentSection = document.getElementById("commentSection");
+  commentSection.innerHTML = "";
+
+  comments.forEach(function (comment) {
+    var commentElement = document.createElement("div");
+    commentElement.className = "comment";
+
+    var authorText = document.createElement("p");
+    if (comment.author.trim() === "") {
+      authorText.textContent = "닉네임을 쓰지 않았을 때 기본값";
+    } else {
+      authorText.textContent = "닉네임: " + comment.author;
+    }
+
+    if (comment.isEditing) {
+      var commentInput = document.createElement("textarea");
+      commentInput.id = "commentText_" + comment.id;
+      commentInput.value = comment.text;
+
+      var saveButton = document.createElement("button");
+      saveButton.className = "btn btn-primary btn-sm";
+      saveButton.textContent = "저장";
+      saveButton.onclick = function () {
+        saveComment(comment.id);
+      };
+
+      var cancelButton = document.createElement("button");
+      cancelButton.className = "btn btn-secondary btn-sm";
+      cancelButton.textContent = "취소";
+      cancelButton.onclick = function () {
+        cancelEdit(comment.id);
+      };
+
+      commentElement.appendChild(authorText);
+      commentElement.appendChild(commentInput);
+      commentElement.appendChild(saveButton);
+      commentElement.appendChild(cancelButton);
+    } else {
+      var commentText = document.createElement("p");
+      if (comment.text.trim() === "") {
+        commentText.textContent = "내용을 쓰지 않았을 때 기본값";
+      } else {
+        commentText.textContent = comment.text;
+      }
+      commentText.id = "commentText_" + comment.id;
+
+      var editButton = document.createElement("button");
+      editButton.className = "btn btn-info btn-sm";
+      editButton.textContent = "수정";
+      editButton.onclick = function () {
+        editComment(comment.id);
+      };
+
+      var deleteButton = document.createElement("button");
+      deleteButton.className = "btn btn-danger btn-sm";
+      deleteButton.textContent = "삭제";
+      deleteButton.onclick = function () {
+        deleteComment(comment.id);
+      };
+
+      commentElement.appendChild(authorText);
+      commentElement.appendChild(commentText);
+      commentElement.appendChild(editButton);
+      commentElement.appendChild(deleteButton);
+    }
+
+    commentSection.appendChild(commentElement);
+  });
+}
+
+// 페이지 로드 시 저장된 댓글 불러오기
+loadComments();
+renderComments();
